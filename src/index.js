fcec7ec10ed5ad4c9cbce2692e22deea73a62cf3
Paweł <102540215+przytop@users.noreply.github.com>
2024-04-09 23:58:49 +0200

    Update index.js
    add closing script to button yourorder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import './sass/main.scss';
 
-// ---SCRIPTS--- //
-document.querySelector('#').addEventListener('click', () => {
-  document.querySelector('#').classList.add('is-closed');
-});
+// ------- SCRIPTS ------- //
+// document.querySelector('#').addEventListener('click', () => {
+//   document.querySelector('#').classList.add('is-closed');
 
-document.querySelector('#').addEventListener('click', () => {
-  document.querySelector('#').classList.remove('is-closed');
+// document.querySelector('#').addEventListener('click', () => {
+//   document.querySelector('#').classList.remove('is-closed');
+
+// --- yourorder --- //
+document.querySelector('#modal-btn-close').addEventListener('click', () => {
+  document.querySelector('#modal-yourorder').classList.add('is-closed');
 });
