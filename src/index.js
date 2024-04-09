0bd6b2ba986f020b72011b73cd0c6afd8c0acfea
Paweł <102540215+przytop@users.noreply.github.com>
2024-04-09 22:59:53 +0200

    Update index.js
    scripts setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './sass/main.scss';
+
+// ---SCRIPTS--- //
+document.querySelector('#').addEventListener('click', () => {
+  document.querySelector('#').classList.add('is-closed');
+});
+
+document.querySelector('#').addEventListener('click', () => {
+  document.querySelector('#').classList.remove('is-closed');
+});
